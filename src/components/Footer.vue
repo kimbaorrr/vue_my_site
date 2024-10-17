9bb7b1363e2488fcb884f788e63ab72c2e6b7038
Bao <nguyenkimbao.0708@gmail.com>
2024-10-17 12:02:38 +0700

    Initial commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,8 +7,8 @@
           <span>&copy; {{ yearOnly }} BaoIT. All rights reserved.</span>
         </div>
         <div class="flex justify-center space-x-4 items-center">
-          <a class="text-gray-700 hover:text-black dark:text-white" :href="infoData.git_url" target="_blank"
-            aria-label="Github">
+          <a class="text-gray-700 hover:text-black dark:text-white dark:hover:text-gray-300"
+            :href="infoData.git_url === '' ? '#' : infoData.git_url" target="_blank" aria-label="Github">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
               <path
@@ -18,16 +18,16 @@
             </svg>
           </a>
 
-          <a class="text-gray-700 hover:text-black dark:text-white" :href="infoData.x_url" target="_blank"
-            aria-label="X">
+          <a class="text-gray-700 hover:text-black dark:text-white dark:hover:text-gray-300"
+            :href="infoData.x_url === '' ? '#' : infoData.x_url" target="_blank" aria-label="X">
             <svg width="24" height="24" viewBox="0 0 1200 1227" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
                 fill="currentColor"></path>
             </svg>
           </a>
-          <a class="text-gray-700 hover:text-black dark:text-white" :href="infoData.linkedin_url" target="_blank"
-            aria-label="Linkedin">
+          <a class="text-gray-700 hover:text-black dark:text-white dark:hover:text-gray-300"
+            :href="infoData.linkedin_url === '' ? '#' : infoData.linkedin_url" target="_blank" aria-label="Linkedin">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
               <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
@@ -35,21 +35,23 @@
               <circle cx="4" cy="4" r="2"></circle>
             </svg>
           </a>
-          <a class="text-gray-700 hover:text-black dark:text-white" :href="'mailto:' + infoData.mail"
-            aria-label="Email">
+          <a class="text-gray-700 hover:text-black dark:text-white dark:hover:text-gray-300"
+            :href="infoData.mail === '' ? '#' : 'mailto:' + infoData.mail" aria-label="Email">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
               <rect width="20" height="16" x="2" y="4" rx="2"></rect>
               <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
             </svg>
           </a>
-          <a class="text-gray-700 hover:text-black dark:text-white" :href="infoData.facebook_url" aria-label="Facebook">
+          <a class="text-gray-700 hover:text-black dark:text-white dark:hover:text-gray-300"
+            :href="infoData.facebook_url === '' ? '#' : infoData.facebook_url" aria-label="Facebook">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
               <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
             </svg>
           </a>
-          <a class="text-gray-700 hover:text-black dark:text-white" :href="infoData.ytb_url" aria-label="Youtube">
+          <a class="text-gray-700 hover:text-black dark:text-white dark:hover:text-gray-300"
+            :href="infoData.ytb_url === '' ? '#' : infoData.ytb_url" aria-label="Youtube">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
               <path
