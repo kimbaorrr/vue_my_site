80213f01b8c5db63654384afdbb17dd2e3099761
Bao <nguyenkimbao.0708@gmail.com>
2024-11-26 11:39:15 +0700

    Add caching feature for fetching JSON data

--- src/components/Mixin.vue
+++ src/components/Mixin.vue
@@ -1,4 +1,5 @@
 <script>
+import { store } from "/src/components/Store.vue"
 export default {
     // Lớp vue hỗn hợp chứa các hàm dùng chung
     name: "Mixin",
@@ -7,34 +8,43 @@ export default {
             /**
              * Lấy danh sách thông tin cá nhân & trả về dạng JSON
              */
+            if (store.resultData.caNhan.length !== 0) {
+                return store.resultData.caNhan;
+            }
             const response = await fetch("https://api.baoit.xyz/my_blog/ca_nhan/get");
             const result = await response.json();
-           
             if (result.success === "true") {
-                const data = await result.data;
-                return data[0] || [];
+                store.resultData.caNhan = await result.data;
+                return store.resultData.caNhan[0] || [];
             }
         },
         async getDuAn() {
             /**
              * * Lấy danh sách thông tin dự án & trả về dạng JSON
              */
+            if (store.resultData.duAn.length !== 0) {
+                return store.resultData.duAn;
+            }
             const response = await fetch("https://api.baoit.xyz/my_blog/du_an/get");
             const result = await response.json();
             if (result.success === "true") {
-                const data = await result.data;
-                return data || [];
+                store.resultData.duAn = await result.data;
+                return store.resultData.duAn || [];
             }
         },
         async getTienIch() {
             /**
              * * Lấy danh sách thông tin tiện ích & trả về dạng JSON
              */
+            if (store.resultData.tienIch.length !== 0) {
+                console.log("OK");
+                return store.resultData.tienIch;
+            }
             const response = await fetch("https://api.baoit.xyz/my_blog/tien_ich/get");
             const result = await response.json();
             if (result.success === "true") {
-                const data = await result.data;
-                return data || [];
+                store.resultData.tienIch = await result.data;
+                return store.resultData.tienIch || [];
             }
         },
         async getLyLich() {
@@ -49,7 +59,7 @@ export default {
             }
             return data[0] || [];
         }
-        
+
     }
 }
 
---
+++ src/components/Store.vue
@@ -0,0 +1,13 @@
+<script>
+import { reactive } from 'vue';
+
+// Lớp lưu trữ trạng thái các biến để cache data thường sử dụng
+export const store = reactive({
+    resultData: {
+        caNhan: [],
+        duAn: [],
+        tienIch: []
+    }
+});
+
+</script>
