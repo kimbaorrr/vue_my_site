a3da1f323c3d21d38c45eede051fa5cfe74df247
Bao <nguyenkimbao.0708@gmail.com>
2025-04-18 12:34:14 +0700

    Bug fixed

--- src/views/DuAn.vue
+++ src/views/DuAn.vue
@@ -27,7 +27,7 @@
     <div class="h-full absolute inset-0 flex justify-center items-center" v-if="isLoading">
       <LoadingSpinner></LoadingSpinner>
     </div>
-
+    
     <div
       class="transform relative p-4 bg-white shadow-lg transition-opacity duration-500 cursor-pointer dark:bg-gray-700 dark:hover:shadow-gray-400 hover:shadow-xl hover:shadow-gray-300 xl:hover:-translate-y-1"
       :class="[
@@ -35,7 +35,7 @@
         isExpanded(item.Id) ? 'max-h-screen overflow-visible' : 'max-h-[24vh] overflow-hidden'
       ]" :data-da-id="item.Id" v-for="(item, idx) in this.filteredProjects" :key="idx" @click="toggleExpand(item.Id)">
 
-
+      
       <!--In Progress Ribbon-->
       <Ribbon :isShowRibbon="!item.Status" :trans="this.trans" :getLang="this.getLang()"></Ribbon>
 
@@ -58,10 +58,50 @@
     </div>
   </div>
 
-  <!--Selected Card Modal-->
-  <SelectedCard :trans="this.trans" :getLang="this.getLang()" :isShowChart="this.isShowChart"
-    :isShowCfs="this.isShowCfs" :selectedCard="this.selectedCard" :projectData="this.projectData"
-    @close="selectedCard = null"></SelectedCard>
+  <!-- Modal for Selected Card -->
+  <div v-if="this.selectedCard" class="bg-opacity-50 fixed inset-0 z-50 flex justify-center items-center bg-black">
+    <div
+      class="w-full max-h-max overflow-y-auto relative p-6 text-sm whitespace-pre-wrap bg-white rounded-lg shadow-2xl dark:bg-gray-800 xl:w-[30%]">
+      <!-- Card Header -->
+      <div class="flex justify-between items-baseline">
+        <h2 class="text-xl font-bold text-black dark:text-white">{{ this.selectedCard.Name }}</h2>
+        <!-- Close Button -->
+        <button class="text-xl text-gray-600 dark:text-gray-300 hover:text-red-600"
+          @click="this.selectedCard = null; this.isShowChart = false;">
+          ✕
+        </button>
+      </div>
+      <!--Card body-->
+      <div class="mt-4 space-y-2 text-sm">
+        <!--Technical Info-->
+        <TechCard :projectData="selectedCard" :trans="trans" :getLang="getLang()" :isShowCfs="isShowCfs"
+          :isShowChart="isShowChart" @closeCfs="isShowCfs = false" @closeChart="isShowChart = false"
+          @toggleChart="isShowChart = !isShowChart" @toggleCfs="isShowCfs = !isShowCfs"></TechCard>
+
+        <!--Card Footer-->
+        <div class="flex justify-between items-center">
+          <div class="flex items-center space-x-1 animate-pulse">
+            <span class="px-2 py-2 rounded-full"
+              :class="this.selectedCard.Status === true ? 'bg-green-600 dark:bg-green-500' : 'bg-red-600 dark:bg-red-500'"></span>
+            <span class="font-semibold"
+              :class="this.selectedCard.Status === true ? 'text-green-600 dark:text-green-500' : 'text-red-600 dark:text-red-500'">{{
+                this.selectedCard.Status
+                  === true ? this.trans?.projects?.filter_by?.done?.[this.getLang()] :
+                  this.trans?.projects?.filter_by?.in_progress?.[this.getLang()] }}</span>
+          </div>
+
+          <button type="button" class="blueBtn px-4 py-2 mt-1 text-sm font-medium text-white"
+            :class="this.selectedCard.TryUrl === '' ? 'opacity-50 cursor-not-allowed' : true"
+            :disabled="this.selectedCard.TryUrl === ''"
+            @click.stop="this.tryProject(this.selectedCard.Id, this.selectedCard.TryUrl)">
+            {{ this.trans?.projects?.card?.try?.[this.getLang()] || "" }}
+          </button>
+        </div>
+
+
+      </div>
+    </div>
+  </div>
 
 
 </template>
@@ -74,7 +114,6 @@ import FilterButtons from "../components/DuAn/FilterButtons.vue";
 import Ribbon from "../components/DuAn/Ribbon.vue";
 import SeeMoreIcon from "../components/DuAn/SeeMoreIcon.vue";
 import TechCard from "../components/DuAn/TechCard.vue";
-import SelectedCard from "../components/DuAn/SelectedCard.vue";
 
 export default {
   name: "DuAn",
@@ -85,8 +124,7 @@ export default {
     FilterButtons,
     Ribbon,
     SeeMoreIcon,
-    TechCard,
-    SelectedCard
+    TechCard
   },
   data() {
     return {
